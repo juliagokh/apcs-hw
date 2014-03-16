37fe99e06401d6e774697ed1760ec44ba4c01c43
Julia Gokhberg <juliagokhberg@Julias-MacBook-Air-2.local>
2014-03-16 19:16:47 -0400

    HW7b added

--- 07-QuickSort/QuickSort2.java
+++ 07-QuickSort/QuickSort2.java
@@ -1,14 +1,12 @@
 import java.util.*;
 import java.io.*;
-import java.util.Random;
 
 public class QuickSort2{
 
-    public static int partition (int[] A, int L, int R){
+    public static Integer[] partition (Integer[] A, int L, int R){
 	int pivotInd;
 	int wall = L;
-	Random rand = new Random();
-	pivotInd = rand.nextInt(L, R+1);
+	pivotInd = L + (int)(Math.random()*(R - L + 1));
 	int pivotVal = A[pivotInd];
 	A[pivotInd] = A[R];
 	A[R] = pivotVal;
@@ -25,5 +23,30 @@ public class QuickSort2{
 		A[x] = temp;
 	    }
 	}
+	A[R] = A[wall];
 	A[wall] = pivotVal;
+	return A;
+    }
+
+    public static Integer[] quickSort(Integer[] A) {
+	if(A.length <= 1) {
+	    return A;
+	}
+	else {
+	    int L = 0;
+	    int R = A.length - 1;
+	    A = partition(A, L, R);
+	    return A;
+	}
+    }
+    
+    public static void main(String[] args) {
+	Integer[] A = {13, 28, 19, 134, 69, 72, 109, 218, 7, 18, 5, 1, 14, 6};
+	Integer[] B = quickSort(A);
+	for(int x = 0; x < B.length; x++) {
+	    System.out.print(B[x] + ", ");
+	}
+    }
+}
+	
 	
