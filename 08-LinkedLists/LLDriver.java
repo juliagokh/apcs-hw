1b8fb418878a1068213095aad2d6a93023c1d121
Julia Gokhberg <juliagokhberg@julias-air-2.78m475.nycboe.org>
2014-03-19 12:13:05 -0400

    MyLinkedList still needs add and remove.

--- 08-LinkedLists/LLDriver.java
+++ 08-LinkedLists/LLDriver.java
@@ -10,5 +10,14 @@ public class LLDriver{
 	System.out.println(L);
 	L.add("Boo");
 	System.out.println(L);
+	System.out.println(L.get(2));
+	System.out.println(L.find("Mike"));
+	System.out.println(L.find("Julia"));
+	System.out.println(L.length());
+	System.out.println(L.toString());
+	System.out.println(L.set(2, "Julia"));
+	System.out.println(L.toString());
+			   
+			   
     }
 }
--- 08-LinkedLists/MyLinkedList.java
+++ 08-LinkedLists/MyLinkedList.java
@@ -1,5 +1,5 @@
 public class MyLinkedList{
-    private Node head;
+    private Node head, temp;
 
     public MyLinkedList(){
 	head = null;
@@ -11,23 +11,80 @@ public class MyLinkedList{
 	temp.setNext(head); //You have to do this first
 	head = temp; // You have to do this second
     }
-    /*
-    public boolean hasNext(Node node){
-	return (node.getNext() == null);
-    }
-    */
     
     public String toString(){
+	//Works
 	String res = "{";
-	Node tmp = new Node("");
-	tmp = head;
-	//I need to figure out how to keep this from crashing.
-	while (tmp.getNext() != null){
-	    res += tmp.getData() + ", ";
-	    tmp = tmp.getNext();
+	temp = head;
+	while (temp != null){
+	    res += temp.getData() + ", ";
+	    temp = temp.getNext();
 	}
 	res += "}";
-	return res;
+	return res;	       
     }
+ 
+
+    public String get (int x){
+	// returns the String @ location x.
+	// Works
+	temp = head;
+	for (int pos = 0; pos < x; pos++){
+	    temp = temp.getNext();
+	}
+	return temp.toString();
+    }
+
+    public int find (String s){
+	// returns the location of the item with value s
+	// or -1 if not found.
+	// Works
+	temp = head;
+	int pos = 0;
+        while (temp != null){
+	    if (temp.getData().equals(s)){
+		return pos;
+	    }else{
+		pos++;
+		temp = temp.getNext();
+	    }
+	}
+	return -1;
+    }
+    
+    public int length(){
+	//Works
+	int counter = 0;
+	temp = head;
+	while (temp != null){
+	    counter++;
+	    temp = temp.getNext();
+	}
+	return counter;
+    }
+    
+    public void add(int x, String s){
+	//adds at a location
+    }
+
+    public String set(int x, String s){
+	//sets location x to String s
+	//it's changing something but not the right thing
+	//Works (as in doesn't crash) but doesn't work correctly
+	temp = head;
+	int pos = 0;
+	String res = "";
+	while (temp != null){
+	    if (pos == x){
+		res = temp.getData();
+		temp.setData(s);
+		return res;
+	    }else{
+		pos++;
+	    }
+	}
+	return "";
+    }
+
     
 }
