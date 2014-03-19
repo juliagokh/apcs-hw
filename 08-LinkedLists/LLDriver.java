a2438ea06c65d10b19ff51c2187b726491577996
Julia Gokhberg <juliagokhberg@Julias-MacBook-Air-2.local>
2014-03-18 21:12:57 -0400

    Needs work

--- 08-LinkedLists/LLDriver.java
+++ 08-LinkedLists/LLDriver.java
@@ -1,6 +1,6 @@
 public class LLDriver{
-    public static void main(Strign[] args){
-	MyLinkedList L = new MyLinkedList():
+    public static void main(String[] args){
+	MyLinkedList L = new MyLinkedList();
 	System.out.println(L);
 	L.add("Sully");
 	System.out.println(L);
--- 08-LinkedLists/MyLinkedList.java
+++ 08-LinkedLists/MyLinkedList.java
@@ -5,15 +5,29 @@ public class MyLinkedList{
 	head = null;
     }
 
-    oublic void add(String d){
+    public void add(String d){
 	Node temp = new Node(d);
 	//Works no matter what (even if the list is empty
 	temp.setNext(head); //You have to do this first
 	head = temp; // You have to do this second
     }
-
-    public String to String(){
-	return ""+head;
+    /*
+    public boolean hasNext(Node node){
+	return (node.getNext() == null);
+    }
+    */
+    
+    public String toString(){
+	String res = "{";
+	Node tmp = new Node("");
+	tmp = head;
+	//I need to figure out how to keep this from crashing.
+	while (tmp.getNext() != null){
+	    res += tmp.getData() + ", ";
+	    tmp = tmp.getNext();
+	}
+	res += "}";
+	return res;
     }
     
 }
